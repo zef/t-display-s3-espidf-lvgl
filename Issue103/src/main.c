2a5364aa70a501520b1569ff6d757b61ce3f3db8
Zef Houssney <zef@zef.studio>
2024-05-22 17:46:21 -0600

    Configure buttons and add idf_component dependencies.

--- Issue103/src/main.c
+++ Issue103/src/main.c
@@ -1,15 +1,53 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "iot_button.h"
+#include "pin_config.h"
 // #include "driver/gpio.h"
 // #include "esp_err.h"
 // #include "esp_log.h"
 
 #include "display.h"
+void button_one(void *arg,void *usr_data) {
+    printf("Button Click!\n");
+    setProgress(20);
+}
+
+void button_two(void *arg,void *usr_data) {
+    printf("Button Two!\n");
+    setProgress(80);
+}
+
+void setup_buttons() {
+    button_config_t gpio_btn_cfg = {
+        .type = BUTTON_TYPE_GPIO,
+        .long_press_time = CONFIG_BUTTON_LONG_PRESS_TIME_MS,
+        .short_press_time = CONFIG_BUTTON_SHORT_PRESS_TIME_MS,
+        .gpio_button_config = {
+            .gpio_num = PIN_BUTTON_1,
+            .active_level = 0,
+        },
+    };
+    button_handle_t gpio_btn = iot_button_create(&gpio_btn_cfg);
+    iot_button_register_cb(gpio_btn, BUTTON_SINGLE_CLICK, button_one, NULL);
+
+    button_config_t gpio_btn2_cfg = {
+        .type = BUTTON_TYPE_GPIO,
+        .long_press_time = CONFIG_BUTTON_LONG_PRESS_TIME_MS,
+        .short_press_time = CONFIG_BUTTON_SHORT_PRESS_TIME_MS,
+        .gpio_button_config = {
+            .gpio_num = PIN_BUTTON_2,
+            .active_level = 0,
+        },
+    };
+    button_handle_t gpio_btn2 = iot_button_create(&gpio_btn2_cfg);
+    iot_button_register_cb(gpio_btn2, BUTTON_SINGLE_CLICK, button_two, NULL);
+}
 
 void app_main(void) {
     vTaskDelay(pdMS_TO_TICKS(2000));
-    startDisplay();
+    start_display();
+    setup_buttons();
 
     while (1) {
         vTaskDelay(pdMS_TO_TICKS(100));
