106d1ee1b855b8282548fc001b1f3c361174cf52
Zef Houssney <zef@zef.studio>
2024-05-24 09:16:23 -0600

    Improve button initialization.

--- src/buttons.c
+++ src/buttons.c
@@ -4,7 +4,7 @@
 #include "buttons.h"
 
 void setup_buttons() {
-    button_config_t gpio_btn_cfg = {
+    button_config_t button_config = {
         .type = BUTTON_TYPE_GPIO,
         .long_press_time = CONFIG_BUTTON_LONG_PRESS_TIME_MS,
         .short_press_time = CONFIG_BUTTON_SHORT_PRESS_TIME_MS,
@@ -13,18 +13,10 @@ void setup_buttons() {
             .active_level = 0,
         },
     };
-    button_handle_t gpio_btn = iot_button_create(&gpio_btn_cfg);
-    iot_button_register_cb(gpio_btn, BUTTON_SINGLE_CLICK, button_one, NULL);
+    button_handle_t b1 = iot_button_create(&button_config);
+    iot_button_register_cb(b1, BUTTON_SINGLE_CLICK, button_one, NULL);
 
-    button_config_t gpio_btn2_cfg = {
-        .type = BUTTON_TYPE_GPIO,
-        .long_press_time = CONFIG_BUTTON_LONG_PRESS_TIME_MS,
-        .short_press_time = CONFIG_BUTTON_SHORT_PRESS_TIME_MS,
-        .gpio_button_config = {
-            .gpio_num = PIN_BUTTON_2,
-            .active_level = 0,
-        },
-    };
-    button_handle_t gpio_btn2 = iot_button_create(&gpio_btn2_cfg);
-    iot_button_register_cb(gpio_btn2, BUTTON_SINGLE_CLICK, button_two, NULL);
+    button_config.gpio_button_config.gpio_num = PIN_BUTTON_2;
+    button_handle_t b2 = iot_button_create(&button_config);
+    iot_button_register_cb(b2, BUTTON_SINGLE_CLICK, button_two, NULL);
 }
